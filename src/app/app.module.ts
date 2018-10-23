26463c8c95736db37592744c0a7f024041fbd14c
Allreview <60billion@allrvw.com>
2018-10-23 14:07:53 +0900

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IamportService } from 'iamport-ionic-kcp';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    IamportService,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { IamportService } from 'iamport-ionic-kcp';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+              public iamport: IamportService,
+              public alert: AlertController
+            ) {
+              
+              
+  }
+  
+  payment(event) {
+    const param = {
+      pay_method : 'card',
+      merchant_uid : 'merchant_' + new Date().getTime(),
+      name : '주문명:결제테스트',
+      amount : 1400,
+      buyer_email : 'iamport@siot.do',
+      buyer_name : '구매자이름',
+      buyer_tel : '010-1234-5678',
+      buyer_addr : '서울특별시 강남구 삼성동',
+      buyer_postcode : '123-456',
+      app_scheme : 'ionickcp'
+    };
+    
+    // 아임포트 관리자 페이지 가입 후 발급된 가맹점 식별코드를 사용
+    this.iamport.payment("imp93881213", param )
+      .then((response)=> {
+        if ( response.isSuccess() ) {
+        }
+      })
+      .catch((err)=> {
+        alert(err)
+      })
+    ;
   }
 
 }
